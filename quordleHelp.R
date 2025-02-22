024927654579c8e8672dc311b05b89da96640d53
Steven Wolf <wolfs15@ecu.edu>
2025-02-21 19:15:39 -0500

    Made interactive function for Quordle.

--- quordleHelp.R
+++ quordleHelp.R
@@ -38,30 +38,64 @@ findCommonWords = function(theGrids){
   }
 }
 
+enterGuess = function(){
+  guess = readline(prompt="Enter guess ")
+  return(guess)
+}
+enterResult = function(n){
+  result = readline(prompt = paste("Enter result for grid",n," "))
+  return(result)
+}
+
+playQuordle = function(){
+  g1 = wordGrid
+  g2 = wordGrid
+  g3 = wordGrid
+  g4 = wordGrid
+  theGrids = list(g1, g2, g3, g4)
+  gridLengths = unlist(lapply(theGrids,length))/5
+  while(!all(gridLengths==1)){
+    theGuess = enterGuess()
+    r1 = enterResult(1)
+    r2 = enterResult(2)
+    r3 = enterResult(3)
+    r4 = enterResult(4)
+    theResult = list(r1, r2, r3, r4)
+    theGrids = elimQuadGrid(theGrids,theGuess,theResult)
+  
+    gridLengths = unlist(lapply(theGrids,length))/5
+    if(!all(gridLengths==1)){
+      minLength = min(gridLengths[gridLengths>1])
+      shortest = which(gridLengths == minLength)
+      for(i in 1:4){
+        print(paste("Grid",i,'has length',gridLengths[i]))
+      }
+    }
+
+    if(all(gridLengths==1)){
+      print(theGrids)
+    } else if(any(gridLengths < 15)){
+      idx = which(gridLengths < 15)
+      for(i in idx){
+        print(paste("Grid ",i,":",sep=''))
+        print(theGrids[[i]])
+      }
+    }
+  }
+}
+
 #############################################################
 # It's good to guess "RAISE" and "DOUBT" as it helps you place vowels 
 # and has a decent number of frequently used consonants.
 
-theGuess = "RAISE"
-theResult=list("XGXYX","YYXXG","XXYYX","YYXXG")
-theGrids = elimQuadGrid(theGrids,theGuess,theResult)
-
-theGuess = "DOUBT"
-theResult=list("XXXXY","GXXXX","XXYXX","XXXXX")
-theGrids = elimQuadGrid(theGrids,theGuess,theResult)
+# theGuess = "RAISE"
+# theResult=list("XGXYX","YYXXG","XXYYX","YYXXG")
+# theGrids = elimQuadGrid(theGrids,theGuess,theResult)
+# 
+# theGuess = "DOUBT"
+# theResult=list("XXXXY","GXXXX","XXYXX","XXXXX")
+# theGrids = elimQuadGrid(theGrids,theGuess,theResult)
 
 # eliminateWords(theGrids[[1]],theGuess,theResult[[1]])
 
 ###########################################################
-gridLengths = unlist(lapply(theGrids,length))/5
-minLength = min(gridLengths[gridLengths>1])
-shortest = which(gridLengths == minLength)
-for(i in 1:4){
-  print(paste("Grid",i,'has length',gridLengths[i]))
-}
-
-if(all(gridLengths==1)){
-  print(theGrids)
-} else {
-  print(theGrids[[shortest]])
-}
